29b3ebcdea015f78acfce5f656a62c42297175a2
Iana Kain <iana.kain@Iana.Kain>
2020-12-08 21:45:36 +0200

    fetch url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
     posts: [],
   }
   componentDidMount() {
-    fetch('http://localhost:3000/posts')
+    fetch('https://feed-sw.herokuapp.com/posts')
       .then(res => res.json())
       .then(({data}) => {
         this.setState({posts: data})
