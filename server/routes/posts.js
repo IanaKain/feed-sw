d01f5803c17e92f19aaae7994cd263fc04d99ff1
Jane <yana.kain@gmail.com>
2020-12-13 18:04:27 +0200

    Dev (#5)
    * add server and dummy data, routes, webpack fix, heroku build test, add test files and Feed component to test deploy, add fonts
    
    Co-authored-by: Iana Kain <iana.kain@Iana.Kain>

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 const router = require('express-promise-router')();
+const FileType = require('file-type');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    data: [
-      {title: 'Morning coffee', content: '10 Ways to Improve Your Morning Coffee. Brew a Better Cup at Home'},
-      {title: 'Signs Their Love is Authentic', content: 'Nothing is possible without love…..For love puts one in a mood to risk everything.'},
-      {title: 'You’re a social perfectionist', content: '6 Psychological Reasons Why You Take Things Too Personally'}
-    ]
-  });
+  fs.readFile(require.resolve(path.resolve(__dirname, '../data/posts.json')), (err, json) => {
+    if (err) {
+      res.status(200).json({ data: [] });
+    }
+    else {
+      const data = JSON.parse(json);
+      const posts = data.map(async (post) => {
+        const image = post.image ? fs.readFileSync(
+          path.resolve(__dirname, `../data/images/${post.image}`)
+        ) : '';
+        const fileData = image.length ? await FileType.fromBuffer(image) : '';
+        const file = fileData ? `data:${fileData.mime};base64,${image.toString('base64')}` : '';
+        return {...post, image: file};
+      })
+
+      Promise.all(posts)
+        .then(result => { res.status(200).json({ data: result }); })
+        .catch((error) => {
+          console.log('Error getting posts', error);
+          return;
+        });
+    }
+  })
 });
 
 module.exports = router;
